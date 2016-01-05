89d71e06e09ed2db8fe0f96e018cca3cae163a95
Ajay <ajay.gopalan81@gmail.com>
2016-01-04 22:21:04 -0800

    first commit

---
+++ chisquare.py
@@ -0,0 +1,19 @@
+from scipy import stats
+import pandas as pd 
+import collections
+import matplotlib.pyplot as plt
+
+# Load the reduced version of the Lending Club Dataset
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+# Drop null rows
+loansData.dropna(inplace=True)
+
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
+
+plt.figure()
+plt.bar(freq.keys(), freq.values(), width=1)
+## plt.show()
+
+sup = chi, p = stats.chisquare(freq.values())
+
+print sup
